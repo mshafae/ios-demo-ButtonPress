23250a3a36b046a0f0b31ddeb2a2f9408861c9fc
Michael Shafae <mshafae@fullerton.edu>
2023-10-19 16:07:44 -0700

    Toggles the view's background color.

--- ButtonPress/ViewController.swift
+++ ButtonPress/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import os
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myButton: UIButton!
+    let logger = Logger()
+    var isToggled = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        logger.log("view did load")
     }
 
+    @IBAction func buttonTapped() {
+        logger.log("The button was pressed")
+        if (isToggled) {
+            self.view.backgroundColor = .red
+        } else {
+            self.view.backgroundColor = .blue
+        }
+        isToggled = !isToggled
+    }
 
 }
 
